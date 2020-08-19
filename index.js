d6ca4d06f65a3dd9c0b480ad76d6e8a9fa1268c4
Stephen Guo <wetech16@gmail.com>
2020-08-18 23:21:17 -0700

    api

--- index.js
+++ index.js
@@ -2,19 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path');
-const members = require('./Members');
-const logger = require('./middleware/logger')
+const logger = require('./middleware/logger');
+const membersApi = require('./routes/api/members');
 
-app.use(logger);
+// app.use(logger);
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/api/members', (req,res) => {res.json(members);});
-//Get Single Member if not found
-app.get('/api/members/:id', (req, res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
-    if (found) { 
-         res.json(members.filter(member => member.id === parseInt(req.params.id)));
-    } else {
-        res.status(404).json( {msg: `not found member id: ${req.params.id} `})
-    }
-})
+app.use(membersApi);
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
