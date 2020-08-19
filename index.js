e00109b4ddd93494bea2bd99bc1de0bc5035b646
Stephen Guo <wetech16@gmail.com>
2020-08-18 21:27:48 -0700

    add dev on script

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req,res) => {
-    res.send('<h1>Hello World </h1>');
+    res.send('<h1>Hello World!! </h1>');
 })
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
