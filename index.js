1e0329c577f7d2fe3a15fa9e9d0657472afc116a
Stephen Guo <wetech16@gmail.com>
2020-08-18 21:32:58 -0700

    sendFile html

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require('path');
 
 app.get('/', (req,res) => {
-    res.send('<h1>Hello World!! </h1>');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
