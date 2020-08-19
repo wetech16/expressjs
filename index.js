ea6bb8a7cb331863d4b98b3305724fe22847e4f6
Stephen Guo <wetech16@gmail.com>
2020-08-18 21:40:44 -0700

    express.static to display all page in public

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path');
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+//localhost:5000/about.html
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
