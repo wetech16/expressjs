450ff1e5592602a1d3937cb1e82ca887833a045e
Stephen Guo <wetech16@gmail.com>
2020-08-18 22:02:47 -0700

    exportmodule

---
+++ Members.js
@@ -0,0 +1,6 @@
+const members = [
+    {id: 1, name: 'Shannon Jackson', email: 'shannon@gmail.com', status: 'active'},
+    {id: 2, name: 'Michael Jacks', email: 'michale@gmail.com', status: 'active'}
+];
+
+module.exports = members
--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path');
+const members = require('./Members');
 
 //localhost:5000/about.html
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Get route to display json
-const members = [
-    {id: 1, name: 'Shannon Jackson', email: 'shannon@gmail.com', status: 'active'},
-    {id: 2, name: 'Michael Jacks', email: 'michale@gmail.com', status: 'active'}
-];
 app.get('/api/members', (req,res) => {res.json(members);});
 //go to Postman to see the json at GET localhost:5000/api/members
 
