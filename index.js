7b4006e70f0afa96902ad65a73eb081051fc5e5e
Stephen Guo <wetech16@gmail.com>
2020-08-18 23:04:38 -0700

    get not found res status

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/api/members', (req,res) => {res.json(members);});
 //Get Single Member
 app.get('/api/members/:id', (req, res) => {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if (found) { 
+         res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    } else {
+        res.status(404).json( {msg: `not found ${req.params.id} `})
+    }
 })
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
