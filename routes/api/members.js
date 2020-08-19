fa79f1abaca4e09fd5fa6295a82864fbd407aaf9
Stephen Guo <wetech16@gmail.com>
2020-08-19 09:41:39 -0700

    push

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const uuid = require('uuid');
 const router = express.Router();
 const members = require('../../Members')
 //Get ALL members
@@ -13,10 +14,20 @@ router.get('/:id', (req, res) => {
     }
 })
 
-//Create member Go to Postman to create a object we can see nothing in response
-//it's because no body parser setup to encode json
+//uuid: random id here is using version 4. npm i uuid
 router.post('/', (req, res) => {
-    res.send(req.body);
+    const newMember = {
+        id: uuid.v4,
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    }
+    //using return so we don't need to specify else
+    if(!newMember.name || !newMember.email) {
+        return res.status(400).json({ msg: 'Please include a name and email'});
+    }
+    members.push(newMember);
+    res.json(members);
 });
 
 module.exports = router;
