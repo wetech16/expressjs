dfc30cb50ac2458f422a76ecedfc964f1477292e
Stephen Guo <wetech16@gmail.com>
2020-08-18 22:57:32 -0700

    get json single member

--- index.js
+++ index.js
@@ -3,20 +3,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path');
 const members = require('./Members');
-const moment = require('moment');
-const momentApp = moment();
+const logger = require('./middleware/logger')
 
-//moment is getting date. .format() will format date 
-const logger = (req, res, next) => {
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}}
-    ${momentApp.format()}`);
-    next();
-}
 app.use(logger);
-
-//localhost:5000/about.html
 app.use(express.static(path.join(__dirname, 'public')));
-//Get route to display json
 app.get('/api/members', (req,res) => {res.json(members);});
-//go to Postman to see the json at GET localhost:5000/api/members
+//Get Single Member
+app.get('/api/members/:id', (req, res) => {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+})
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
