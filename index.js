463538b0aa94c31331f728d4f647d79a9d486953
Stephen Guo <wetech16@gmail.com>
2020-08-18 21:22:57 -0700

    setup app.get route /

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req,res) => {
+    res.send('<h1>Hello World </h1>');
+})
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
