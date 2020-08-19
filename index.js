ac58d22501907911bf18d36d8538f2118ccef2f0
Stephen Guo <wetech16@gmail.com>
2020-08-18 23:33:51 -0700

    -route

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path');
 const logger = require('./middleware/logger');
-const membersApi = require('./routes/api/members');
 
 // app.use(logger);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(membersApi);
+// 2nd way module export of route get json 
+app.use('/api/members', require('./routes/api/members'));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
--- routes/api/members.js
+++ routes/api/members.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const members = require('../../Members')
-
-router.get('/api/members', (req,res) => {res.json(members);});
+//Get ALL members
+router.get('/', (req,res) => {res.json(members);});
 //Get Single Member if not found
-router.get('/api/members/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     const found = members.some(member => member.id === parseInt(req.params.id));
     if (found) { 
          res.json(members.filter(member => member.id === parseInt(req.params.id)));
